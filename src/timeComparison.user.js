34789d2cc770e8fe2b1fa4ea6ad70f7fabcccf09
kentakojima <kenta.int.tech@gmail.com>
2024-03-24 16:11:53 +0900

    修正

--- src/timeComparison.user.js
+++ src/timeComparison.user.js
@@ -642,7 +642,7 @@
         ticketPreviewBlock.appendChild(textarea);
 
         function outputText(kindCheckList, selectCheckListDict, textarea, kindList, raceName) {
-            var numToUmaban = { 1: "①", 2: "②", 3: "③", 4: "④", 5: "⑤", 6: "⑥", 7: "⑦", 8: "⑧", 9: "⑨", 10: "⓾", 11: "⑪", 12: "⑫", 13: "⑬", 14: "⑭", 15: "⑮", 16: "⑯", 17: "⑰", 18: "⑱" }
+            var numToUmaban = { 1: "①", 2: "②", 3: "③", 4: "④", 5: "⑤", 6: "⑥", 7: "⑦", 8: "⑧", 9: "⑨", 10: "⑩", 11: "⑪", 12: "⑫", 13: "⑬", 14: "⑭", 15: "⑮", 16: "⑯", 17: "⑰", 18: "⑱" }
             textarea.innerHTML = "";
             var s = raceName + "\n";
             for (let i = 0; i < kindList.length; i++) {
